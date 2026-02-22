6ed73825c3fd8dd3275b9336ddbbd3b18cf4ad66
Alberto <alu0101627137@ull.edu.es>
2026-02-22 15:32:54 +0000

    Nombre modificado de fichero memoria_instrucciones -> memoria_programa

---
+++ ram-machine-simulator/include/memoria_programa.h
@@ -0,0 +1,44 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Diseño y Analisis de Algoritmos 2025-2026
+  * 3º Año de Carrera
+  * Práctica 2: Experimentación y complejidad computacional -
+  *             Patrón Estrategia - Simulador de la máquina RAM
+  *
+  * @author Alberto Del Castillo Díaz alu0101627137@ull.edu.es
+  * @author Bruno Morales Hernandez alu0101664309@ull.edu.es
+  * @brief 
+  * @date 
+  * @version 1.0
+  */
+
+#ifndef PRACTICARAM_INCLUDE_MEMORIA_INSTRUCCIONES_H
+#define PRACTICARAM_INCLUDE_MEMORIA_INSTRUCCIONES_H
+
+#include <iostream>
+#include <vector>
+#include <memory> // Uso de punteros inteligentes los cuales nos ahorran el tener que limpiar manualmente
+                  // std::unique_ptr<> y std::make_unique<>()
+#include "instruccion.h"
+
+/** 
+ * Clase MemoriaInstrucciones
+ *       Guarda el conjunto de instrucciones de un programa. Esta
+ *       memoria no es modificable.
+ * 
+ */
+class MemoriaPrograma {
+ public:
+  MemoriaPrograma() = default;
+  ~MemoriaPrograma() = default;
+  void AgregarInstruccion(std::unique_ptr<Instruccion>);
+  Instruccion* ObtenerInstruccion(size_t) const;
+  size_t Tamano() const { return instrucciones_.size(); }
+  void MostrarMemoria() const;
+ private:
+  std::vector<std::unique_ptr<Instruccion>> instrucciones_;
+};
+
+#endif
